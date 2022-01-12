cfe1e9fd97f75c1c82668e0390c35f3bf1f43010
Shami <shamitomita@gmail.com>
2022-01-12 16:36:11 -0500

    minor housekeeping

--- app/controllers/api/v1/favorited_articles_controller.rb
+++ app/controllers/api/v1/favorited_articles_controller.rb
@@ -1,3 +1,16 @@
 class Api::V1::FavoritedArticlesController < ApplicationController
-  
+  def new
+    favorited_article = FavoritedArticle.new
+  end
+
+  def create
+    favorited_article = FavoritedArticle.new(favorited_article_params)
+  end
+
+  private
+  def user_params
+    params.require(:favorited_article).permit(:user_id, :article_id)
+  end
+
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.new(user_params)
+    if !user.save
+      render json: {:errors => user.errors.full_messages}
+    else
+      render json: article
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :zipcode)
+  end
+
+
 end
