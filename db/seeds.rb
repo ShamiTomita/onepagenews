39730450c0252f9d2c83afde2fbb212737f4191a
ShamiTomita <shamitomita@gmail.com>
2022-04-14 22:25:20 -0400

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ require 'area'
 newsapi = News.new("942d82be5ab04c0e810e412394d04c7a")
 sources = newsapi.get_sources(country: 'us', language: 'en')
 #business entertainment general health science sports technology
-all_articles = newsapi.get_everything(sources: 'bbc-news,the-verge,nbc-news,cnn,reuters,politico,abc-news,NPR', language: 'en', from: '2021-12-20')
+all_articles = newsapi.get_everything(sources: 'bbc-news,the-verge,nbc-news,cnn,reuters,politico,abc-news,NPR', language: 'en', from: '2022-04-01')
 #use this to go through first
 business_articles = newsapi.get_top_headlines(category: 'business', language: 'en', country:'us')
 entertainment_articles = newsapi.get_top_headlines(category: 'entertainment', language: 'en', country:'us')
