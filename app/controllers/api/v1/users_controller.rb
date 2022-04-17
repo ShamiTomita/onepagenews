9b52a29f19c56d64b4a716e6dafbf2b740b66e90
ShamiTomita <shamitomita@gmail.com>
2022-04-17 17:13:37 -0400

    update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,12 +4,15 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    user = User.all
+  end 
   def create
     user = User.new(user_params)
     if !user.save
       render json: {:errors => user.errors.full_messages}
     else
-      render json: article
+      render json: user
     end
   end
 
